0575636971dd71cf9a5673db635b9703a5ea42fb
Eman Rabbani <emanrabbani041@gmail.com>
2025-06-24 23:34:14 -0400

    Test 2

--- src/main.cpp
+++ src/main.cpp
@@ -260,3 +260,8 @@ void opcontrol() {
     pros::delay(ez::util::DELAY_TIME);  // This is used for timer calculations!  Keep this ez::util::DELAY_TIME
   }
 }
+
+void bigCat()
+{
+   std::cout << "big cat";
+}
